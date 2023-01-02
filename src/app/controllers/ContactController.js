ef22e9d40d81bd9060b550eb0bb9e279e3410b96
Walter Jaworski <walter.jaworski@gmail.com>
2023-01-02 18:32:20 -0300

    initial commit

---
+++ src/app/controllers/ContactController.js
@@ -0,0 +1,43 @@
+const ContactsRepository = require('../repositories/ContactsRepository');
+
+class ContactController {
+  async index(request, response) {
+    const contacts = await ContactsRepository.findAll();
+
+    response.json(contacts);
+  }
+
+  async show(request, response) {
+    const { id } = request.params;
+    const contact = await ContactsRepository.findById(id);
+
+    if (!contact) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+
+    response.json(contact);
+  }
+
+  store() {
+    // criar novo registro
+  }
+
+  update() {
+    // atualizar registro
+  }
+
+  async delete(request, response) {
+    const { id } = request.params;
+
+    const contact = await ContactsRepository.findById(id);
+
+    if (!contact) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+
+    await ContactsRepository.delete(id);
+    response.sendStatus(204);
+  }
+}
+
+module.exports = new ContactController();
---
+++ src/app/repositories/ContactsRepository.js
@@ -0,0 +1,41 @@
+const { v4 } = require('uuid');
+
+let contacts = [
+  {
+    id: v4(),
+    name: 'Walter',
+    email: 'walter.jaworski@gmail.com',
+    phone: '12121212',
+    category_id: v4(),
+  },
+  {
+    id: v4(),
+    name: 'Matheus',
+    email: 'matheus.jaworski@gmail.com',
+    phone: '12121212',
+    category_id: v4(),
+  },
+];
+
+class ContactsRepository {
+  findAll() {
+    return new Promise((resolve) => {
+      resolve(contacts);
+    });
+  }
+
+  findById(id) {
+    return new Promise((resolve) => resolve(
+      contacts.find((contact) => contact.id === id),
+    ));
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
+    });
+  }
+}
+
+module.exports = new ContactsRepository();
